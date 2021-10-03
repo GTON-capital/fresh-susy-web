12e528569623c51982af3209dcf6f3152ffd5996
cvaize <cvaize@gmail.com>
2021-10-03 18:01:54 +0500

    (Feat) Work

--- components/Landing/FifthBlock.vue
+++ components/Landing/FifthBlock.vue
@@ -1,5 +1,49 @@
 <template>
-  <div>
+  <div class="text-center sm:text-left">
+    <div class="font-bold text-[100px] md:text-[160px] font-heading italic bg-clip-text text-transparent bg-solana mb-[20px] mb-[40px] leading-none">
+      $SUSY token
+    </div>
+    <div class="flex flex-wrap mx-[-20px] lg:mx-0 text-[18px] sm:text-[30px]">
+
+      <div class="w-full sm:w-1/2 md:w-1/3 px-[20px] lg:w-[260px] lg:pr-[42px] lg:pl-0 mb-[44px]">
+        <div class="w-[108px] h-[108px] mb-[10px] sm:mb-[28px] relative mx-auto sm:mx-0">
+          <img class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" src="~/assets/img/landing/token/1.svg" alt="">
+        </div>
+        Launch
+        in Q4 2021
+      </div>
+
+      <div class="w-full sm:w-1/2 md:w-1/3 px-[20px] lg:w-[310px] lg:pr-[42px] lg:pl-0 mb-[44px]">
+        <div class="w-[108px] h-[108px] mb-[10px] sm:mb-[28px] relative mx-auto sm:mx-0">
+          <img class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" src="~/assets/img/landing/token/2.svg" alt="">
+        </div>
+        Fee discounts
+        for bridge trxs for $SUSY holders
+      </div>
+
+      <div class="w-full sm:w-1/2 md:w-1/3 px-[20px] lg:w-[172px] lg:pr-0 lg:pl-0 mb-[44px]">
+        <div class="w-[108px] h-[108px] mb-[10px] sm:mb-[28px] relative mx-auto sm:mx-0">
+          <img class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" src="~/assets/img/landing/token/3.svg" alt="">
+        </div>
+        Multichain: 7 chains
+        at launch
+      </div>
+
+      <div class="w-full sm:w-1/2 px-[20px] lg:w-[423px] lg:pr-[86px] lg:pl-0 mb-[44px]">
+        <div class="w-[108px] h-[108px] mb-[10px] sm:mb-[28px] relative mx-auto sm:mx-0">
+          <img class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" src="~/assets/img/landing/token/4.svg" alt="">
+        </div>
+        Boosted liquidity: paired to GTON across all established chains and most popular DEXs
+      </div>
+
+      <div class="w-full sm:w-1/2 px-[20px] lg:w-[272px] lg:pr-0 lg:pl-0 mb-[44px]">
+        <div class="w-[108px] h-[108px] mb-[10px] sm:mb-[28px] relative mx-auto sm:mx-0">
+          <img class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" src="~/assets/img/landing/token/5.svg" alt="">
+        </div>
+        Staking $SUSY grants validator’s rights
+      </div>
+
+    </div>
   </div>
 </template>
 
